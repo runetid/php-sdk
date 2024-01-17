318cb16904e226699c0305e86c95d0f57ec7c92b
fgorsky <fedor@support-pc.org>
2024-01-17 11:42:20 +0300

    user fields

--- src/models/User.php
+++ src/models/User.php
@@ -11,6 +11,7 @@ class User implements ModelInterface
     public $runet_id;
     public $photo;
     public $birthay;
+    public $email;
 
     public function load(\ArrayAccess|array $data): ModelInterface
     {
