07125a25e33487e079c59a062b9e6a41fb1e2a50
fgorsky <fedor@support-pc.org>
2024-02-16 10:09:18 +0300

    search event participant by email

--- src/facade/Event.php
+++ src/facade/Event.php
@@ -130,4 +130,45 @@ class Event extends Facade
 
         return $result;
     }
+
+    public function searchParticipant($email): ?array
+    {
+        $params = [
+            'limit' => 1,
+            'offset' => 0,
+            'pagination' => [
+                'page' => 1,
+                'perPage' => 1
+            ],
+            'filter' => [
+                'email' => $email,
+            ],
+        ];
+
+        $url ='/event/participant/list?'.http_build_query($params);
+
+        $response = $this->client->request($url, 'GET');
+
+        if ($response->getStatusCode() !== 200) {
+            return null;
+        }
+
+        $decode = json_decode($response->getBody(), true);
+
+        if (false === isset($decode['data'])) {
+            return null;
+        }
+
+        $result = [];
+
+        foreach ($decode['data'] as $item) {
+
+            $model = new \runetid\sdk\models\User();
+            $model->load($item);
+
+            $result[] = $model;
+        }
+
+        return $result;
+    }
 }
